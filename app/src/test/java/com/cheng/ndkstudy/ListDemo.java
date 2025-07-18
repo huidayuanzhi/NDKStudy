d2a7c060125419f518d7b254184214c518277dd1
liwangcheng <liwangcheng@mobiuspace.com>
2025-07-18 09:08:46 +0800

    mergeTwoLists

--- app/src/test/java/com/cheng/ndkstudy/ListDemo.java
+++ app/src/test/java/com/cheng/ndkstudy/ListDemo.java
@@ -225,6 +225,48 @@ public class ListDemo {
     }
 
     /**
+     * 21. 合并两个有序链表
+     * 将两个升序链表合并为一个新的升序链表并返回。
+     */
+    // 方法一：递归
+    public static ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists1(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists1(list1, list2.next);
+            return list2;
+        }
+    }
+
+    // 方法二：迭代
+    public static ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        ListNode dummyHead = new ListNode(-1);
+        ListNode prev = dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                prev.next = list1;
+                list1 = list1.next;
+            } else {
+                prev.next = list2;
+                list2 = list2.next;
+            }
+            prev = prev.next;
+        }
+        if (list1 != null) {
+            prev.next = list1;
+        }
+        if (list2 != null) {
+            prev.next = list2;
+        }
+        return dummyHead.next;
+    }
+
+    /**
      * 25. K 个一组翻转链表
      * 如果节点总数不是 k 的整数倍，那么将最后剩余的节点保持原有顺序。
      * 示例 1：
