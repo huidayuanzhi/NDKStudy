02c2c43a50b65de6faab56cada15411e741dd508
liwangcheng <liwangcheng@mobiuspace.com>
2025-07-21 16:48:54 +0800

    isInterleave

--- app/src/test/java/com/cheng/ndkstudy/StringDemo.java
+++ app/src/test/java/com/cheng/ndkstudy/StringDemo.java
@@ -538,4 +538,63 @@ public class StringDemo {
         return ans;
     }
 
+    /**
+     * 97. 交错字符串
+     * 示例 1：
+     * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+     * 输出：true
+     * 示例 2：
+     * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+     * 输出：false
+     */
+    // 方法一：动态规划
+    public static boolean isInterleave1(String s1, String s2, String s3) {
+        if (s1 == null || s2 == null || s3 == null) return false;
+        int n = s1.length();
+        int m = s2.length();
+        int t = s3.length();
+        if (n + m != t) {
+            return false;
+        }
+        boolean[][] f = new boolean[n + 1][m + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    f[i][j] = f[i][j] || (f[i - 1][j] && s1.charAt(i - 1) == s3.charAt(p));
+                }
+                if (j > 0) {
+                    f[i][j] = f[i][j] || (f[i][j - 1] && s2.charAt(j - 1) == s3.charAt(p));
+                }
+            }
+        }
+        return f[n][m];
+    }
+
+    // 动态规划 + 滚动数组
+    public static boolean isInterleave2(String s1, String s2, String s3) {
+        if (s1 == null || s2 == null || s3 == null) return false;
+        int n = s1.length();
+        int m = s2.length();
+        int t = s3.length();
+        if (n + m != t) {
+            return false;
+        }
+        boolean[] f = new boolean[m + 1];
+        f[0] = true;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    f[j] = f[j] && s1.charAt(i - 1) == s3.charAt(p);
+                }
+                if (j > 0) {
+                    f[j] = f[j] || s2.charAt(j - 1) == s3.charAt(p);
+                }
+            }
+        }
+        return f[m];
+    }
+
 }
