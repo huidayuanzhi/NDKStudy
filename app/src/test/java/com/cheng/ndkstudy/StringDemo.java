ddecbadc5b275cbb0fa4a89206560b7901c50e02
liwangcheng <liwangcheng@mobiuspace.com>
2025-07-21 14:30:24 +0800

    checkInclusion

--- app/src/test/java/com/cheng/ndkstudy/StringDemo.java
+++ app/src/test/java/com/cheng/ndkstudy/StringDemo.java
@@ -290,4 +290,110 @@ public class StringDemo {
         return res;
     }
 
+    /**
+     * 567.字符串的排列
+     * 给你两个字符串s1和s2，写一个函数来判断s2是否包含s1的排列。换句话说，s1的排列之一是s2的子串。
+     * 示例 1：
+     * 输入：s1 = "ab" s2 = "eidbaooo"
+     * 输出：true
+     * 解释：s2 包含 s1 的排列之一 ("ba").
+     * 示例 2：
+     * 输入：s1= "ab" s2 = "eidboaoo"
+     * 输出：false
+     */
+    // 方法一：滑动窗口
+    public static boolean checkInclusion1(String s1, String s2) {
+        if (s1 == null || s2 == null || s2.length() < s1.length()) return false;
+        int n = s1.length();
+        int m = s2.length();
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (int i = 0; i < n; i++) {
+            cnt1[s1.charAt(i) - 'a']++;
+            cnt2[s2.charAt(i) - 'a']++;
+        }
+        if (Arrays.equals(cnt1, cnt2)) {
+            return true;
+        }
+        // 注意 i 是从 n 开始的，也就是说第 i 个字符是没有被计数的，所以是进入计数(++)
+        for (int i = n; i < m; i++) {
+            ++cnt2[s2.charAt(i) - 'a'];
+            --cnt2[s2.charAt(i - n) - 'a'];
+            if (Arrays.equals(cnt1, cnt2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 优化
+    public static boolean checkInclusion2(String s1, String s2) {
+        if (s1 == null || s2 == null || s2.length() < s1.length()) return false;
+        int n = s1.length();
+        int m = s2.length();
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; i++) {
+            --cnt[s1.charAt(i) - 'a'];
+            ++cnt[s2.charAt(i) - 'a'];
+        }
+        int diff = 0;
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] != 0) {
+                diff++;
+            }
+        }
+        if (diff == 0) {
+            return true;
+        }
+        for (int i = n; i < m; i++) {
+            int x = s2.charAt(i) - 'a';
+            int y = s2.charAt(i - n) - 'a';
+            if (x == y) {
+                continue;
+            }
+            if (cnt[x] == 0) {
+                diff++;
+            }
+            cnt[x]++;
+            if (cnt[x] == 0) {
+                diff--;
+            }
+            if (cnt[y] == 0) {
+                diff++;
+            }
+            cnt[y]--;
+            if (cnt[y] == 0) {
+                diff--;
+            }
+            if (diff == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 方法二：双指针
+    public static boolean checkInclusion3(String s1, String s2) {
+        if (s1 == null || s2 == null || s2.length() < s1.length()) return false;
+        int n = s1.length();
+        int m = s2.length();
+        int[] cnt = new int[26];
+        for (int i = 0; i < n; i++) {
+            cnt[s1.charAt(i) - 'a']--;
+        }
+        int left = 0;
+        for (int right = 0; right < m; right++) {
+            int x = s2.charAt(right) - 'a';
+            ++cnt[x];
+            while (cnt[x] > 0) {
+                --cnt[s2.charAt(left) - 'a'];
+                left++;
+            }
+            if (right - left + 1 == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
