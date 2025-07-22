a4221c03d1669db5962ee7e21f021f17c6085456
liwangcheng <liwangcheng@mobiuspace.com>
2025-07-22 10:51:45 +0800

    nextGreaterElement2

--- app/src/test/java/com/cheng/ndkstudy/StackDemo.java
+++ app/src/test/java/com/cheng/ndkstudy/StackDemo.java
@@ -1,6 +1,62 @@
 package com.cheng.ndkstudy;
 
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class StackDemo {
 
+    /**
+     * 496. 下一个更大元素 I
+     * nums1 中数字 x 的 下一个更大元素是指 x 在 nums2 中对应位置右侧的第一个比 x 大的元素。
+     * 示例 1：
+     * 输入：nums1 = [4,1,2], nums2 = [1,3,4,2].
+     * 输出：[-1,3,-1]
+     * 示例 2：
+     * 输入：nums1 = [2,4], nums2 = [1,2,3,4].
+     * 输出：[3,-1]
+     */
+    // 方法一：暴力
+    public static int[] nextGreaterElement1(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null || nums1.length <= 0 || nums2.length <= 0) return new int[0];
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] res = new int[m];
+        for (int i = 0; i < m; i++) {
+            int j = 0;
+            while (j < n && nums2[j] != nums1[i]) {
+                j++;
+            }
+            int k = j + 1;
+            while (k < n && nums2[k] < nums1[i]) {
+                k++;
+            }
+            res[i] = k < n ? nums2[k] : -1;
+        }
+        return res;
+    }
+
+    // 方法二：单调栈 + 哈希表
+    public static int[] nextGreaterElement2(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null || nums1.length <= 0 || nums2.length <= 0) return new int[0];
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] res = new int[m];
+        Map<Integer, Integer> map = new HashMap<>();
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = n - 1; i >= 0; i--) {
+            int num = nums2[i];
+            while (!stack.isEmpty() && num >= stack.peek()) {
+                stack.pop();
+            }
+            map.put(num, stack.isEmpty() ? -1 : stack.peek());
+            stack.push(num);
+        }
+        for (int i = 0; i < m; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+        return res;
+    }
 
 }
--- app/src/test/java/com/cheng/ndkstudy/StackTest.java
+++ app/src/test/java/com/cheng/ndkstudy/StackTest.java
@@ -1,4 +1,41 @@
 package com.cheng.ndkstudy;
 
+import org.junit.Test;
+
 public class StackTest {
+
+    @Test
+    public void testNextGreaterElement1() {
+        Logger.log("nextGreaterElement1");
+        int[] result = StackDemo.nextGreaterElement1(
+                new int[]{4,1,2},
+                new int[]{1,3,4,2}
+        ); // -1,3,-1
+        Logger.log("nextGreaterElement1 result = ");
+        Logger.log(result);
+        int[] result2 = StackDemo.nextGreaterElement1(
+                new int[]{2,4},
+                new int[]{1,2,3,4}
+        ); // 3,-1
+        Logger.log("nextGreaterElement1 result = ");
+        Logger.log(result2);
+    }
+
+    @Test
+    public void testNextGreaterElement2() {
+        Logger.log("nextGreaterElement2");
+        int[] result = StackDemo.nextGreaterElement2(
+                new int[]{4,1,2},
+                new int[]{1,3,4,2}
+        ); // -1,3,-1
+        Logger.log("nextGreaterElement2 result = ");
+        Logger.log(result);
+        int[] result2 = StackDemo.nextGreaterElement2(
+                new int[]{2,4},
+                new int[]{1,2,3,4}
+        ); // 3,-1
+        Logger.log("nextGreaterElement2 result = ");
+        Logger.log(result2);
+    }
+
 }
