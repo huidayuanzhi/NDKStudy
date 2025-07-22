36e4c9f144fbf89f50150a0d0f67892b2818c483
liwangcheng <liwangcheng@mobiuspace.com>
2025-07-22 10:01:17 +0800

    removeKdigits

--- app/src/test/java/com/cheng/ndkstudy/StringDemo.java
+++ app/src/test/java/com/cheng/ndkstudy/StringDemo.java
@@ -3,8 +3,10 @@ package com.cheng.ndkstudy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -787,6 +789,47 @@ public class StringDemo {
             }
         }
         return dp[m][n];
+    }
+
+    /**
+     * 402. 移掉 K 位数字
+     * 给你一个以字符串表示的非负整数 num 和一个整数 k ，移除这个数中的 k 位数字，使得剩下的数字最小。请你以字符串形式返回这个最小的数字。
+     * 示例 1 ：
+     * 输入：num = "1432219", k = 3
+     * 输出："1219"
+     * 解释：移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219 。
+     * 示例 2 ：
+     * 输入：num = "10200", k = 1
+     * 输出："200"
+     * 解释：移掉首位的 1 剩下的数字为 200. 注意输出不能有任何前导零。
+     * 示例 3 ：
+     * 输入：num = "10", k = 2
+     * 输出："0"
+     */
+    // 方法一：贪心 + 单调栈
+    public static String removeKdigits(String num, int k) {
+        if (num == null || k < 1 || num.length() <= k) return "0";
+        int length = num.length();
+        Deque<Character> deque = new LinkedList<>();
+        for (int i = 0; i < length; i++) {
+            char digit = num.charAt(i);
+            while (!deque.isEmpty() && k > 0 && deque.peekLast() > digit) {
+                deque.pollLast();
+                k--;
+            }
+            deque.offerLast(digit);
+        }
+        for (int i = 0; i < k; i++) {
+            deque.pollLast();
+        }
+        while (deque.peekFirst() == '0') {
+            deque.pollFirst();
+        }
+        StringBuilder builder = new StringBuilder();
+        while (!deque.isEmpty()) {
+            builder.append(deque.pollFirst());
+        }
+        return builder.isEmpty() ? "0" : builder.toString();
     }
 
 }
