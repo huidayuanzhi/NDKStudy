1c2f27ea00a93fef26b366386d2ac03cf6302b46
liwangcheng <liwangcheng@mobiuspace.com>
2025-07-22 10:32:57 +0800

    MinStack

---
+++ app/src/test/java/com/cheng/ndkstudy/MinStack.java
@@ -0,0 +1,38 @@
+package com.cheng.ndkstudy;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 155. 最小栈
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ */
+class MinStack {
+
+    // 方法一：辅助栈
+    private Deque<Integer> xStack;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<>();
+        minStack = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        xStack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+    
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return xStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
--- app/src/test/java/com/cheng/ndkstudy/StackDemo.java
+++ app/src/test/java/com/cheng/ndkstudy/StackDemo.java
@@ -1,4 +1,6 @@
 package com.cheng.ndkstudy;
 
 public class StackDemo {
+
+
 }
