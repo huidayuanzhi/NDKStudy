2188b04e70c9d9d98e85536aaf56724ff41c7337
liwangcheng <liwangcheng@mobiuspace.com>
2025-07-24 14:38:36 +0800

    minDepth

--- app/src/test/java/com/cheng/ndkstudy/BinaryTreeDemo.java
+++ app/src/test/java/com/cheng/ndkstudy/BinaryTreeDemo.java
@@ -1,5 +1,8 @@
 package com.cheng.ndkstudy;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class BinaryTreeDemo {
 
      public static class TreeNode {
@@ -29,6 +32,58 @@ public class BinaryTreeDemo {
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
         return Math.max(left, right) + 1;
+    }
+
+    /**
+     * 111. 二叉树的最小深度
+     * 示例 1：
+     * 输入：root = [3,9,20,null,null,15,7]
+     * 输出：2
+     * 示例 2：
+     * 输入：root = [2,null,3,null,4,null,5,null,6]
+     * 输出：5
+     */
+    // 方法一：深度优先搜索
+    public static int minDepth1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int minDepth = Integer.MAX_VALUE;
+        if (root.left != null) {
+            minDepth = Math.min(minDepth, minDepth1(root.left));
+        }
+        if (root.right != null) {
+            minDepth = Math.min(minDepth, minDepth1(root.right));
+        }
+        return minDepth + 1;
+    }
+
+    // 迭代
+    public static int minDepth2(TreeNode root) {
+        if (root == null) return 0;
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int minDepth = 1;
+        while (!queue.isEmpty()) {
+            int length = queue.size();
+            for (int i = 0; i < length; i++) {
+                TreeNode currNode = queue.poll();
+                if (currNode.left == null && currNode.right == null) {
+                    return minDepth;
+                }
+                if (currNode.left != null) {
+                    queue.offer(currNode.left);
+                }
+                if (currNode.right != null) {
+                    queue.offer(currNode.right);
+                }
+            }
+            minDepth++;
+        }
+        return minDepth;
     }
 
 }
--- app/src/test/java/com/cheng/ndkstudy/BinaryTreeTest.java
+++ app/src/test/java/com/cheng/ndkstudy/BinaryTreeTest.java
@@ -2,19 +2,46 @@ package com.cheng.ndkstudy;
 
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTreeTest {
 
-    private BinaryTreeDemo.TreeNode create(Integer[] nums) {
+    private BinaryTreeDemo.TreeNode createComplete(Integer[] nums) {
         if (nums == null || nums.length <= 0) return null;
         return buildTreeHelper(nums, 0);
     }
 
     private BinaryTreeDemo.TreeNode buildTreeHelper(Integer[] nums, int index) {
-        if (index >= nums.length) return null; // 越界检查
+        if (index >= nums.length || nums[index] == null) return null; // 越界检查
         BinaryTreeDemo.TreeNode node = new BinaryTreeDemo.TreeNode(nums[index]); // 创建当前节点
         node.left = buildTreeHelper(nums, 2 * index + 1); // 左子节点，对应2*index+1
         node.right = buildTreeHelper(nums, 2 * index + 2); // 右子节点，对应2*index+2
         return node;
+    }
+
+    private BinaryTreeDemo.TreeNode create(Integer[] arr) {
+        if (arr == null || arr.length == 0 || arr[0] == null) {
+            return null;
+        }
+        Queue<BinaryTreeDemo.TreeNode> queue = new LinkedList<>();
+        BinaryTreeDemo.TreeNode root = new BinaryTreeDemo.TreeNode(arr[0]);
+        queue.offer(root);
+        int index = 1;
+        while (!queue.isEmpty() && index < arr.length) {
+            BinaryTreeDemo.TreeNode current = queue.poll();
+            if (index < arr.length && arr[index] != null) {
+                current.left = new BinaryTreeDemo.TreeNode(arr[index]);
+                queue.offer(current.left);
+            }
+            index++;
+            if (index < arr.length && arr[index] != null) {
+                current.right = new BinaryTreeDemo.TreeNode(arr[index]);
+                queue.offer(current.right);
+            }
+            index++;
+        }
+        return root;
     }
 
     @Test
@@ -27,5 +54,26 @@ public class BinaryTreeTest {
         Logger.log("maxDepth result = " + result); // 2
     }
 
+    @Test
+    public void testMinDepth1() {
+        Logger.log("minDepth1");
+        BinaryTreeDemo.TreeNode root1 = create(new Integer[]{3,9,20,null,null,15,7});
+        int result = BinaryTreeDemo.minDepth1(root1);
+        Logger.log("minDepth1 result = " + result); // 2
+        BinaryTreeDemo.TreeNode root2 = create(new Integer[]{2,null,3,null,4,null,5,null,6});
+        result = BinaryTreeDemo.minDepth1(root2);
+        Logger.log("minDepth1 result = " + result); //5
+    }
+
+    @Test
+    public void testMinDepth2() {
+        Logger.log("minDepth2");
+        BinaryTreeDemo.TreeNode root1 = create(new Integer[]{3,9,20,null,null,15,7});
+        int result = BinaryTreeDemo.minDepth2(root1);
+        Logger.log("minDepth2 result = " + result); // 2
+        BinaryTreeDemo.TreeNode root2 = create(new Integer[]{2,null,3,null,4,null,5,null,6});
+        result = BinaryTreeDemo.minDepth2(root2);
+        Logger.log("minDepth2 result = " + result); //5
+    }
 
 }
